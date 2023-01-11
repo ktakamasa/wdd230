962dc2c79314afab6de6d2c35b7ba6ce52bf19d0
kohtakamasa <kohtakamasa@gmail.com>
2023-01-11 11:04:30 +0000

    started hamburger menu for portal

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,12 @@ document.getElementById("year").innerHTML = currentYear;
 //get last modified date
 let lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
+
+// Navigation / Hamburger menu
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+
+menubutton.addEventListener("click", () => {
+	menuitems.forEach((item) => item.classList.toggle("open"));
+	menubutton.classList.toggle("close");
+});
