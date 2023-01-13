d2ee15326d7b4eb1379bfa30678082ac489fdefa
kohtakamasa <kohtakamasa@gmail.com>
2023-01-13 10:29:38 +0000

    chamber of commerce home page small view

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,12 @@
 //get last modified date
 let lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = lastModified;
+
+// responsive hamburger menu
+const menubutton= document.querySelector("#menu-button");
+const menuitems = document.querySelector("nav ul");
+
+menubutton.addEventListener("click", () => {
+    menubutton.classList.toggle("open");
+    menuitems.classList.toggle("open");
+});
