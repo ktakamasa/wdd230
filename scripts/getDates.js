2cd8dcaf20006b8244a599ab1fc7a3cb26df299e
kohtakamasa <kohtakamasa@gmail.com>
2023-01-16 17:19:23 +0000

    portal page responsive images, visit counter, maps

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -23,3 +23,23 @@ modeButton.addEventListener("click", () => {
 	modeButton.classList.toggle("dark");
 	mainArea.classList.toggle("dark");
 })
+
+// visit counter
+const displayVisits = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	displayVisits
+.textContent = numVisits;
+} else {
+	displayVisits
+.textContent = "This is your first visit!";
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
