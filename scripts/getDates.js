b447666cd393f3ffa911c8cfb2eaec3207b813bc
kohtakamasa <kohtakamasa@gmail.com>
2023-01-26 23:46:45 +0000

    forms and tables

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -40,3 +40,69 @@ if (numVisits !== 0) {
 numVisits++;
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
+
+//Form 
+//slider
+const rating = document.querySelector("#rating");
+const rangeValue = document.querySelector("#r");
+
+function displayRatingValue() {
+  rating.textContent = rangeValue.value;
+}
+
+rangeValue.addEventListener("change", displayRatingValue);
+rangeValue.addEventListener("input", displayRatingValue);
+
+//username
+const user1 = document.querySelector("#username1");
+const user2 = document.querySelector("#username2");
+const message = document.querySelector("#usernameMessage");
+
+user2.addEventListener("focusout", checkSameUsername);
+
+function checkSameUsername() {
+  if (user1.value !== user2.value) {
+    message.textContent = "Usernames DO NOT MATCH!";
+    message.style.display = "block";
+    user2.style.backgroundColor = "red";
+    user2.style.color = "#fff";
+    user2.focus();
+    user2.value = "";
+  } else {
+    message.style.display = "none";
+    user2.style.backgroundColor = "#fff";
+    user2.style.color = "#000";
+  }
+}
+
+//submit form
+const submitBtn = document.querySelector("#submitBtn");
+const submitReview = document.querySelector(".submitReview tbody");
+const reviewForm = document.querySelector("#reviewForm");
+
+document.querySelector("#reviewForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+submitBtn.addEventListener("click", () => {
+  submitReview.textContent = "";
+  const inputName = document.querySelector("input[name=fname]");
+  addLines("Name", inputName.value);
+  const inputEmail = document.querySelector("input[name=email]");
+  addLines("Email", inputEmail.value);
+  const inputRating = document.querySelector("input[type=range]");
+  addLines("Rating", inputRating.value);
+  const inputUsername = document.querySelector("input[name=username1]");
+  addLines("Username", inputUsername.value);
+});
+
+function addLines(field, value) {
+  const column1 = document.createElement("td");
+  column1.textContent = field;
+  const column2 = document.createElement("td");
+  column2.textContent = value;
+  const row = document.createElement("tr");
+  row.appendChild(column1);
+  row.appendChild(column2);
+  submitReview.appendChild(row);
+}
