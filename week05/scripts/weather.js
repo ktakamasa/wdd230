4174d5f5c34c17b6f9bfcd7a2a7a544b1d2f3aec
kohtakamasa <kohtakamasa@gmail.com>
2023-01-31 16:31:46 +0000

    w05 prep activities

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&appid=58f0d0f204c9a8f0d01dbf9e766b5458&units=imperial';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); //testing
+      displayResults(data); 
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
+}
