c0b77c725eaa6bc39aea84c9503102d9208e993d
kohtakamasa <kohtakamasa@gmail.com>
2023-02-06 23:50:47 +0000

    fixes

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,11 +1,8 @@
 const lat = "40.12553412958195";
 const lon = "-83.16034569661029";
 const part = "minutely,hourly,alerts";
-<<<<<<< HEAD
-const key = "437209d363a4387bb2f5dffcea58bbee";
-=======
 const key = "58f0d0f204c9a8f0d01dbf9e766b5458";
->>>>>>> a33db27 (fix error and add chamber)
+
 const urlWeather = `//api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${key}&units=imperial`;
 
 const currentTemp = document.querySelector("#current-temp");
@@ -39,7 +36,7 @@ function displayWeather(data) {
   weatherIcon.setAttribute("alt", desc);
   captionDesc.textContent = `${desc}`;
 
-  //forecast 
+  //forecast
   const highs = document.querySelector("#forecastHigh");
   const lows = document.querySelector("#forecastLow");
   // const expected = document.querySelector("#expectedWeather");
@@ -61,5 +58,4 @@ function displayWeather(data) {
     lows.appendChild(dayLow);
     // expected.appendChild(dayWeather);
   }
-
 }
