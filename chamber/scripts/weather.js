e415c0a97904883c5d27e85fa9c0c98dce8fb05f
kohtakamasa <kohtakamasa@gmail.com>
2023-02-07 10:36:25 +0000

    chamber

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,61 +1,74 @@
 const lat = "40.12553412958195";
 const lon = "-83.16034569661029";
-const part = "minutely,hourly,alerts";
 const key = "58f0d0f204c9a8f0d01dbf9e766b5458";
 
-const urlWeather = `//api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${key}&units=imperial`;
+const urlCurrent = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+const urlForecast = `//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
 
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("#weatherDesc");
 
-async function getWeather() {
+//current weather
+async function getCurrentWeather() {
   try {
-    const response = await fetch(urlWeather);
-    if (response.ok) {
-      const data = await response.json();
-      // console.log(data); //testing
-      displayWeather(data);
+    const responseCurrent = await fetch(urlCurrent);
+    if (responseCurrent.ok) {
+      const dataCurrent = await responseCurrent.json();
+      // console.log(dataCurrent); //testing
+      displayCurrentWeather(dataCurrent);
     } else {
-      throw Error(await response.text());
+      throw Error(await responseCurrent.text());
     }
   } catch (error) {
     console.log(error);
   }
 }
+getCurrentWeather();
 
-getWeather();
+//forecast weather
+async function getForecastWeather() {
+  try {
+    const responseForecast = await fetch(urlForecast);
+    if (responseForecast.ok) {
+      const dataForecast = await responseForecast.json();
+      // console.log(dataForecast); //testing
+      displayForecastWeather(dataForecast);
+    } else {
+      throw Error(await responseForecast.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getForecastWeather();
 
-function displayWeather(data) {
-  // console.table(data);
-  //current
-  currentTemp.innerHTML = `${data.current.temp}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
-  let desc = data.current.weather[0].description;
+function displayCurrentWeather(data) {
+  // console.log(data);
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", desc);
   captionDesc.textContent = `${desc}`;
+}
 
-  //forecast
+function displayForecastWeather(data) {
+  // console.log(data);
   const highs = document.querySelector("#forecastHigh");
   const lows = document.querySelector("#forecastLow");
-  // const expected = document.querySelector("#expectedWeather");
 
   for (let i = 0; i < 3; i++) {
     // console.table(data.daily[i]);
     const dayHigh = document.createElement("td");
     const dayLow = document.createElement("td");
-    // const dayWeather = document.createElement("td");
 
-    dayHigh.innerHTML = `${data.daily[i].temp.max}&deg;F`;
+    dayHigh.innerHTML = `${data.list[i].main.temp_max}&deg;F`;
     // console.log(dayHigh);
-    dayLow.innerHTML = `${data.daily[i].temp.min}&deg;F`;
+    dayLow.innerHTML = `${data.list[i].main.temp_min}&deg;F`;
     // console.log(dayLow);
-    // dayWeather.innerHTML = `${data.daily[i].weather[0].description}`;
-    // console.log(dayWeather);
 
     highs.appendChild(dayHigh);
     lows.appendChild(dayLow);
-    // expected.appendChild(dayWeather);
   }
 }
